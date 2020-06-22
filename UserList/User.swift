6db5356f6abcdc31667ca9de8feb57b6f122e50f
yazzy4 <yazburrell@pursuit.org>
2020-06-22 17:16:10 -0400

    JSON data appearing in tableview

---
+++ UserList/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  UserList
+//
+//  Created by Yaz Burrell on 6/22/20.
+//  Copyright © 2020 Yaz Burrell. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    var city: String
+}
--- UserList/UsersVC.swift
+++ UserList/UsersVC.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class UsersVC: UITableViewController {
 
-    
-    let tempUsers = ["Jane", "Silvia", "George", "Dorkus", "Sabrina", "Harvey"]
+  var users = [User]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         getUsers()
     }
+ 
     
     func getUsers(){
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
@@ -26,24 +26,58 @@ class UsersVC: UITableViewController {
         let dataTask = session.dataTask(with: url) { (data, response, error) in
             
             if let httpResponse = response as? HTTPURLResponse {
-                
                 print(httpResponse.statusCode)
-                 
+            }
+            if let error = error {
+                print(error.localizedDescription)
+                
+            } else if let data = data {
+
+                do {
+                    guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [[String:Any]] else
+                    { return }
+                    print(json)
+                    
+                    let users = [User]()
+                    
+                    for userDictionary in json {
+                        
+                        guard
+                            let name = userDictionary["name"] as? String,
+                            let address = userDictionary["address"] as? [String : Any],
+                            let city = address["city"] as? String
+                        else { return }
+                  
+                        let user = User(name: name, city: city)
+                        self.users.append(user)
+                        
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                        }
+                        
+                    }
+                    
+                    print(users)
+                    
+                } catch let err{
+                    print(err)
+                }
             }
         }
         dataTask.resume()
     }
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      
-        return tempUsers.count
+        return users.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        let user = tempUsers[indexPath.row]
-        cell.textLabel?.text = user
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        let user = users[indexPath.row]
+        cell.textLabel?.text = user.name
+        cell.detailTextLabel?.text = user.city
         return cell
     }
 
